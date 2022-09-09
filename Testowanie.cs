2087b00d917bf0dfc975354efb9a8f7cc2a2486a
snoxy <snoxycoa@gmail.com>
2022-09-09 17:49:50 +0200

    Added new class Testowanie

---
+++ Testowanie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dron
+{
+    class Testowanie
+    {
+        public void Test()
+        {
+            Console.WriteLine("Na potrzeby gita");
+        }
+    }
+}
