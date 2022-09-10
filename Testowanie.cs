b40e315216c339278ab99483aa4fc7aa691b8d71
snoxy <snoxycoa@gmail.com>
2022-09-10 12:11:06 +0200

    Fixed too much comments

--- Testowanie.cs
+++ Testowanie.cs
@@ -8,17 +8,7 @@ namespace Dron
         {
             //Here is the new line of comment
             Console.WriteLine("Na potrzeby gita");
-            //This is comment from master branch
-            //Now i am on test branch
-            //Kolejne zmiany na gałęzi test
-            //Już powoli rozumiem jak to działa - z gałezi test
-            //Teraz komentarz bez wyychania na remote test
-            //Kolejny komentarz bez wypychania na repote test
-
-            //komentarz z innego punktu na commicie
-            //Chce wykonać innego merge
-
-            //Sam commit z test
+            
         }
     }
 }
